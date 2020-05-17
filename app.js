f81fb6895945b1194af508f11d028a59cdcd0dd7
Petrimus <petrimus76@gmail.com>
2020-05-17 21:40:12 +0300

    ready for google

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
 const axios = require('axios')
 const mcache = require('memory-cache')
 
@@ -11,7 +9,7 @@ const baseUrl = 'https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/'
 
 //
 app.use(cors())
-// app.use(express.static('build'))
+app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
@@ -36,8 +34,6 @@ const cache = (duration) => {
   }
 }
 
-// app.use(middleware.errorHandler)
-
 app.get('/', (req, res) => {
   res.send('<h1>Hello You</h1>')
 })
@@ -52,43 +48,40 @@ app.get('/api/hospitalised', cache(100000), async (req, res) => {
   const response = await axios.get(`${baseUrl}finnishCoronaHospitalData`)
   const dataToSort = response.data['hospitalised']
   console.log('axios called')
-
-  // const lastDay = dataToSort[dataToSort.length - 1].date
+ 
   const result = dataToSort.filter(day => day.area === 'Finland')
   result.forEach(day => day.date = Date.parse(day.date))
 
   res.send(result)
 })
 
-app.get('api/deaths', cache(100000), async (req, res) => {
+app.get('/api/deaths',  cache(10000), async (req, res) => {
 const response = await axios.get(`${baseUrl}finnishCoronaData/v2`)
 const deaths = response.data['deaths']
 
-res.send(deaths)
+const result = deaths.reduce((ary, item) => {
+  const finded = ary.find(row => row.date === item.date)
+  if (finded) {
+    finded.value ++
+  } else {
+    const obj = {
+      date: item.date,
+      value: 1
+    }
+    ary.push(obj)
+  }
+  return ary
+}, [])
+result.sort((day1, day2) => {
+  return Date.parse(day1.date) - Date.parse(day2.date)
+})
+
+res.send(result)
 })
 
 app.use(middleware.unknownEndpoint)
 
 module.exports = app
 
-
-/*
- const firstResult = response.data
-console.log('firstResult', firstResult)
-  const result = response.data.reduce((ary, item) => {
-    const foundElement = ary.find(el => el.date === item.date)
-    if (foundElement) {
-      foundElement.value + 1
-    } else {
-      const newElement = {
-        date: item.date,
-        value: 1
-      }
-      ary.push(newElement)
-    }
-    return ary
-  }, [])
-  res.send(result)
-*/
 
 
